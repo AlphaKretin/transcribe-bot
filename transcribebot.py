dc814427d6da766a8f98c4fdf41b6f71b83ceb47
Molly Finley <molly@mlfinley.com>
2024-02-08 21:07:37 -0500

    Add error handling and traceback for file saving and transcribing

--- transcribebot.py
+++ transcribebot.py
@@ -2,6 +2,7 @@ import whisper
 import discord
 import os
 from dotenv import load_dotenv
+import traceback
 load_dotenv()
 
 model = whisper.load_model("base")
@@ -19,17 +20,21 @@ class MyClient(discord.Client):
                     file_name = attachment.filename
                     # add message id to file name
                     file_name = f'{message.id}-{file_name}'
-                    # save the file
-                    await attachment.save(file_name)
-                    print(f'Saved attachment: {file_name}')
-                    text = whisper.transcribe(model, file_name)
-                    transcribed_text = text['text'].strip()
-                    # get the users nickname
-                    server_nickname = message.guild.get_member(message.author.id).display_name
-                    reply_message = f"**{server_nickname}**: {transcribed_text}"
-                    await message.reply(reply_message)
-                    # delete the file
-                    os.remove(file_name)
+                    try:
+                        # save the file
+                        await attachment.save(file_name)
+                        print(f'Saved attachment: {file_name}')
+                        text = whisper.transcribe(model, file_name)
+                        transcribed_text = text['text'].strip()
+                        # get the users nickname
+                        server_nickname = message.guild.get_member(message.author.id).display_name
+                        reply_message = f"**{server_nickname}**: {transcribed_text}"
+                        await message.reply(reply_message)
+                    except Exception as e:
+                        traceback.print_exc()
+                    finally:
+                        # delete the file
+                        os.remove(file_name)
                     
 intents = discord.Intents.default()
 intents.message_content = True
